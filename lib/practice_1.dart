1724aa62c5ac101d4178fdd499a267b1b00b12a7
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-01-03 12:56:27 +0100

    Using Stack with position widget

--- lib/practice_1.dart
+++ lib/practice_1.dart
@@ -5,19 +5,32 @@ class Practice_One extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget main = Scaffold(
-      appBar: AppBar(title: const Text('Stack')),
-    );
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        main,
-        const Banner(message: 'Top Start', location: BannerLocation.topStart),
-        const Banner(message: 'Top End', location: BannerLocation.topEnd),
-        const Banner(
-            message: 'Bottom Start', location: BannerLocation.bottomStart),
-        const Banner(message: 'Bottom End', location: BannerLocation.bottomEnd),
-      ],
-    );
+    // Widget main = Scaffold(
+    //   appBar: AppBar(title: const Text('Stack')),
+    // );
+    // return Stack(
+    //   fit: StackFit.expand,
+    //   children: const [
+    //     main,
+    //     Banner(message: 'Top Start', location: BannerLocation.topStart),
+    //     Banner(message: 'Top End', location: BannerLocation.topEnd),
+    //     Banner(message: 'Bottom Start', location: BannerLocation.bottomStart),
+    //     Banner(message: 'Bottom End', location: BannerLocation.bottomEnd),
+    //   ],
+    // );
+
+    return Scaffold(
+        appBar: AppBar(title: const Text('Stack')),
+        body: Stack(
+          fit: StackFit.expand,
+          children: const [
+            Material(color: Colors.yellowAccent),
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Icon(Icons.star, size: 50),
+            )
+          ],
+        ));
   }
 }
