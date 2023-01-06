a1b354194ff8de526eb21b4cd59ecc333f2b953d
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-01-06 23:43:39 +0100

    Updating implicit with animation container

--- lib/animation/implicit.dart
+++ lib/animation/implicit.dart
@@ -11,9 +11,10 @@ class _implicit_animationState extends State<implicit_animation> {
   @override
   Widget build(BuildContext context) {
     return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Container(
-        width: 30,
-        height: 30,
+      AnimatedContainer(
+        width: 100,
+        height: 100,
+        duration: const Duration(seconds: 120),
         decoration: BoxDecoration(
           color: Colors.green,
           borderRadius: BorderRadius.circular(10),
